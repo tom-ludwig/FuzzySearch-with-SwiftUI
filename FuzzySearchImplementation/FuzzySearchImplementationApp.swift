56bbb63da2ca0d8a307555593917bb482c3d58b4
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-04-30 18:19:43 +0200

    Initial Commit

---
+++ FuzzySearchImplementation/FuzzySearchImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuzzySearchImplementationApp.swift
+//  FuzzySearchImplementation
+//
+//  Created by Tommy Ludwig on 30.04.23.
+//
+
+import SwiftUI
+
+@main
+struct FuzzySearchImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
