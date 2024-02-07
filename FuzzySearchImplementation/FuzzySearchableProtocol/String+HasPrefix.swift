c68f5ab9438a25b2e204b39e1dae0096c8f9ea59
Tom Ludwig <tommludwig@icloud.com>
2024-02-07 17:49:09 +0100

    Update String+HasPrefix.swift

--- FuzzySearchImplementation/FuzzySearchableProtocol/String+HasPrefix.swift
+++ FuzzySearchImplementation/FuzzySearchableProtocol/String+HasPrefix.swift
@@ -13,6 +13,10 @@ extension String {
     ///   - prefix: The FuzzySearchCharacter whose content or normalised content to check for a prefix match.
     ///   - index: The index from which to start searching for the prefix.
     /// - Returns: The length of the matching prefix, or nil if no match is found.
+    /// - Note: This function could also be named hasPrefix, 
+    ///   as it deals solely with single-character inputs, 
+    ///   making a boolean return type equally appropriate. For our use case that is.
+    ///   If you pass more than single characters you need to use the Int return value.
     func lengthOfMatchingPrefix(prefix: FuzzySearchCharacter, startingAt index: Int) -> Int? {
         guard let stringIndex = self.index(self.startIndex, offsetBy: index, limitedBy: self.endIndex) else {
             return nil
